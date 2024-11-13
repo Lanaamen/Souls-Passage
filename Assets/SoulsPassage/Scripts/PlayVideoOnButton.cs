b756c941be283f0dd4d7671932cfe576c5de1f00
Lanaamen <143440284+Lanaamen@users.noreply.github.com>
2024-11-13 11:14:41 +0100

    video workds

---
+++ Assets/SoulsPassage/Scripts/PlayVideoOnButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideoOnButton : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    public void PlayVideo()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.Play();
+        }
+    }
+}
